64d56546d4fe4be07862b35c1dee419e24c0a3c2
Mathuba Dlamini <Mathuba58@gmail.com>
2019-06-14 19:55:07 +0200

    Added code to read and setup undirected graph

--- bipartite.py
+++ bipartite.py
@@ -1,20 +1,44 @@
 #Uses python3
 
 import sys
-import queue
+from collections import deque
+
+
+def add_edge(graph, vertex1, vertex2):
+    """Build edges of graph by adding vertex2 to addjacent list of vertex1.
+    
+    Vertex1 is also added to vertex2 adjacent list. If vertex1 is not in
+    the graph, vertex1 is added as new key.
+    
+    Parameters
+    ----------
+    graph: dictionary
+        The graph being constructed as adjacency list.
+    vertex1, vertex2:int
+        vertex1 is the key and vertex2 is the neighbour to vertex 1
+        all values greater than 0.
+    """
+
+    edges = (vertex1, vertex2)
+    (v1, v2) = (edges)
+    if v1 in graph:
+        if v2 not in graph[v1]:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+        else:
+            graph[v1] = v2
+    
 
 def bipartite(adj):
     #write your code here
     return -1
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(bipartite(adj))
+    n, m = map(int, sys.stdin.readline().split())
+    graph = {vertex: [] for vertex in range(1, n+1)}
+    for _ in range(m):
+        vert1, vert2 = map(int, sys.stdin.readline().split())
+        add_edge(graph, vert1, vert2)
+
+    # print(bipartite(adj))
+    print(graph)
